4b8fc282e4c3defe082ae3b85c9e84ea296afe8f
Ginkuimain <enzofigueiredofeitosa@outlook.com>
2023-10-21 12:44:48 -0300

    Versão de Linux para eu poder editar durante as aulas

--- paciente.py
+++ paciente.py
@@ -2,17 +2,18 @@ import os.path
 import shutil
 
 
-def validar():
+def validar():  # Validar o diretório
     CWD = os.getcwd()
     os.chdir(fr'{CWD}')
     dst_path = 'pacientes'
     if not os.path.exists(dst_path):
         os.makedirs(dst_path)
+
+
 validar()
 
-
 cwd = os.getcwd()
-os.chdir(f'{cwd}\pacientes')  # Qual o diretório que estamos usando. CWD = Current Working Directory
+os.chdir(f'{cwd}/pacientes')  # Qual o diretório que estamos usando. CWD = Current Working Directory
 
 
 def createPatient():
@@ -33,7 +34,7 @@ def visualisePatients():
     print("")
 
     if os.path.exists(f"{name}.txt"):
-        with open(f"{name}.txt", "r") as file: # Ler o txt, fazendo uso do "r" (read)
+        with open(f"{name}.txt", "r") as file:  # Ler o txt, fazendo uso do "r" (read)
             print(file.read())
             print("")
     else:
@@ -73,18 +74,20 @@ def archivePatient():
 
     name = str(input("Digite o nome do paciente que será arquivado: "))
     if os.path.exists(f"{name}.txt"):
-        shutil.move(fr"{cwd}\pacientes\{name}.txt",fr"{cwd}\pacientes\pacientesArquivados\{name}.txt")  # Mover o .txt do paciente com uso do shutil (só está aqui para isso mesmo)
+        shutil.move(fr"{cwd}/pacientes/{name}.txt",
+                    fr"{cwd}/pacientes/pacientesArquivados/{name}.txt")  # Mover o .txt do paciente com uso do shutil (só está aqui para isso mesmo)
         print("\n---> Paciente arquivado com sucesso! \n")
     else:
         print('Paciente não encontrado.')
 
 
 def deArchivePatient():
-    os.chdir(fr'{cwd}\pacientes\pacientesArquivados')
+    os.chdir(fr'{cwd}/pacientes/pacientesArquivados')
     name = str(input("Digite o nome do paciente que será arquivado: "))
 
     if os.path.exists(f"{name}.txt"):
-        shutil.move(fr"{cwd}\pacientes\pacientesArquivados\{name}.txt", fr"{cwd}\pacientes\{name}.txt") # Mover o .txt do paciente com uso do shutil (só está aqui para isso mesmo)
+        shutil.move(fr"{cwd}/pacientes/pacientesArquivados/{name}.txt",
+                    fr"{cwd}/pacientes/{name}.txt")  # Mover o .txt do paciente com uso do shutil (só está aqui para isso mesmo)
         print("\n---> Paciente desarquivado com sucesso! \n")
     else:
         print('Paciente não encontrado.')
