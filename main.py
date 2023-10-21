1e02ff38952398195dc89439682b33718d8d6bd6
ENZO <enzofigueiredofeitosa@outlook.com>
2023-10-21 04:31:21 -0300

    Literalmente fiz a porra toda. Agora vou jogar war thunder 👍
    ( Façam a documentação :3 )

---
+++ main.py
@@ -0,0 +1,44 @@
+from paciente import *
+
+
+def main():
+    while True:
+        try:
+            print("Qual é a operação desejada? \n"
+                  "1. Criar novo paciente. \n"
+                  "2. Vizualizar paciente. \n"
+                  "3. Atualizar descrição. \n"
+                  "4. Remover um paciente. \n"
+                  "5. Arquivar um paciente. \n"
+                  "6. Desarquivar um paciente \n"
+                  "C. Cancelar e parar o programa. \n")
+            usrAnswer = str(input("Digite aqui: "))  # Perguntar ao usuário oque ele quer
+            print("Pegando informações... \n ")
+
+            match usrAnswer.lower():
+                case "1":
+                    createPatient()  # Criar paciente
+                case "2":
+                    visualisePatients()  # Vizualizar paciente
+                case "3":
+                    editPatients()  # Editar descrição/histórico do paciente
+                case "4":
+                    removePatients()  # Remover um paciente
+                case "5":
+                    archivePatient()  # Arquivar um paciente
+                case "6":
+                    deArchivePatient()  # Desarquivar um paciente
+                case "c":
+                    print("--->Parando o programa. como desejado")
+                    break
+                case _:
+                    print(
+                        "--->Por favor, selecione umad as opções.\n")  # Caso "Default", para qualquer coisa que não esteja na listagem
+
+        except Exception as erro:  # Caso algo dessa bomba dê merda
+            print("Algo deu errado. Por faor, tente novamente~!")
+            print(f"O erro em questão, caso você seja meio nerd: {erro}")
+
+
+if __name__ == '__main__':  # Main
+    main()
---
+++ paciente.py
@@ -0,0 +1,81 @@
+import os.path
+import shutil
+cwd = os.getcwd()
+os.chdir(f'{cwd}\pacientes')  # Qual o diretório que estamos usando. CWD = Current Working Directory
+
+
+def createPatient():
+    name = str(input("Digite o nome do paciente: "))
+
+    if os.path.exists(f"{name}.txt"):
+        return print("\nUsuário já existente\n")  # Se o usuário existe ou não
+
+    desc = str(input("Digite a condição/descrição do paciente: "))
+    with open(f"{name}.txt", "w") as file:  # Criar um novo txt, fazendo o uso do "w" (write)
+        file.write(f"Nome do Paciente: {name} \n")
+        file.write(f"Descrição do paciente: {desc} \n")
+        print("\n---> Usuário adicionado com sucesso! \n")
+
+
+def visualisePatients():
+    name = str(input("Digite o nome do paciente que será vizualizado: "))
+    print("")
+
+    if os.path.exists(f"{name}.txt"):
+        with open(f"{name}.txt", "r") as file: # Ler o txt, fazendo uso do "r" (read)
+            print(file.read())
+            print("")
+    else:
+        print("---> Paciente não encontrado. \n")
+
+
+def editPatients():
+    name = str(input("Digite o nome do paciente que será editado: "))
+    descAlter = str(input("Nova descrição: "))
+
+    if os.path.exists(f'{name}.txt'):
+        with open(f'{name}.txt', 'r') as f:  # editar a descrição do sujeito, na mesma lógica do adicionar um novo
+            lines = f.readlines()
+        lines[1] = f"Descrição do paciente: {descAlter} \n"
+
+        with open(f'{name}.txt', 'w') as f:
+            f.writelines(lines)
+            print("---> Descrição mudada com sucesso!!")
+    else:
+        print('Paciente não encontrado.')
+
+
+def removePatients():
+    name = str(input("Digite o nome do paciente que será deletado: "))
+
+    if os.path.exists(f"{name}.txt"):  # Validar se usuário existe para deletar
+        os.remove(f'{name}.txt')
+        print("\n---> Paciente removido com sucesso! \n")
+    else:
+        print("Usuário não encontrado.")
+
+
+def archivePatient():
+    name = str(input("Digite o nome do paciente que será arquivado: "))
+    dst_path = 'pacientesArquivados'
+
+
+    if not os.path.exists(dst_path):
+        os.makedirs(dst_path)
+
+    if os.path.exists(f"{name}.txt"):
+        shutil.move(fr"{cwd}\pacientes\{name}.txt",fr"{cwd}\pacientes\pacientesArquivados\{name}.txt")  # Mover o .txt do paciente com uso do shutil (só está aqui para isso mesmo)
+        print("\n---> Paciente arquivado com sucesso! \n")
+    else:
+        print('Paciente não encontrado.')
+
+
+def deArchivePatient():
+    os.chdir(fr'{cwd}\pacientes\pacientesArquivados')
+    name = str(input("Digite o nome do paciente que será arquivado: "))
+
+    if os.path.exists(f"{name}.txt"):
+        shutil.move(fr"{cwd}\pacientes\pacientesArquivados\{name}.txt", fr"{cwd}\pacientes\{name}.txt") # Mover o .txt do paciente com uso do shutil (só está aqui para isso mesmo)
+        print("\n---> Paciente desarquivado com sucesso! \n")
+    else:
+        print('Paciente não encontrado.')
